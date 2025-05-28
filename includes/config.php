2c8b39812e95c0f7dab7ce9b69ca8190f80ef8ee
Michał Czubiliński <90175012+czu365@users.noreply.github.com>
2025-05-28 13:35:37 +0200

    add missing files

---
+++ includes/config.php
@@ -0,0 +1,10 @@
+<?php
+// Konfiguracja bazy danych
+define('DB_HOST', 'localhost');
+define('DB_USER', 'calendar_user');
+define('DB_PASS', 'twoje_mocne_haslo');
+define('DB_NAME', 'calendar_app');
+
+// Konfiguracja aplikacji
+define('BASE_URL', 'http://twoja_domena.com');
+define('SITE_NAME', 'Mój Kalendarz');
